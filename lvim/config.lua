cc64e7ee0994927c25dc3832e97a25ea637c47e7
akshar <glaivesharp@gmail.com>
2023-09-07 22:05:31 +0530

    Add keybinds for terminal

--- lvim/config.lua
+++ lvim/config.lua
@@ -8,6 +8,14 @@ lvim.colorscheme = "catppuccin-macchiato"
 -- Setters
 vim.opt.relativenumber = true
 
+-- keybinds 
+lvim.builtin.which_key.mappings["t"] = {
+  name = "+Terminal",
+  f = { "<cmd>ToggleTerm<cr>", "Floating terminal" },
+  v = { "<cmd>2ToggleTerm size=30 direction=vertical<cr>", "Split vertical" },
+  h = { "<cmd>2ToggleTerm size=30 direction=horizontal<cr>", "Split horizontal" },
+}
+
 -- Additional Plugins
 lvim.plugins = {
   { 
@@ -18,7 +26,19 @@ lvim.plugins = {
       vim.g.copilot_assume_mapped = true
       vim.g.copilot_no_tab_map = true
     end,
-  },
+  },{
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  }, 
   { "lunarvim/colorschemes" },
   { "catppuccin/nvim" },
 }

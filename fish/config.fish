8d80b4d22d98f7bb7dd4d0a1de4cf2ab45a18840
akshar <glaivesharp@gmail.com>
2023-09-07 22:04:11 +0530

    Add new alias and show icons on ls

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,10 @@
-alias ls='exa --group-directories-first'
-alias la='exa -a'
-alias lt="exa --tree -D -L 2 -I 'ache|log|logs|node_modules|vendor'"
-alias ltt="exa --tree -D -L 3 -I 'ache|log|logs|node_modules|vendor'"
-alias lttt="exa --tree -D -L 4 -I 'ache|log|logs|node_modules|vendor'"
-alias ltttt="exa --tree -D -L 5 -I 'ache|log|logs|node_modules|vendor'"
+alias cat='bat'
+alias ls='exa --group-directories-first --icons'
+alias la='exa -a --icons'
+alias lt="exa --tree -D -L 2 -I 'ache|log|logs|node_modules|vendor' --icons"
+alias ltt="exa --tree -D -L 3 -I 'ache|log|logs|node_modules|vendor' --icons" 
+alias lttt="exa --tree -D -L 4 -I 'ache|log|logs|node_modules|vendor' --icons"
+alias ltttt="exa --tree -D -L 5 -I 'ache|log|logs|node_modules|vendor' --icons"
 
 if status is-interactive
     # Commands to run in interactive sessions can go here

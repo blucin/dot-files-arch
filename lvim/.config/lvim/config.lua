b428983ca7bf1a7f69ca7488e8f633644b6f423a
akshar <glaivesharp@gmail.com>
2024-07-12 19:29:36 +0530

    Update lvim config

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -3,81 +3,98 @@
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
 
-lvim.colorscheme = "catppuccin-mocha"
+lvim.colorscheme = "sweet-fusion"
 
 vim.g.copilot_filetypes = { markdown = true } -- false by default
 vim.opt.guifont = "Iosevka:h17"
 vim.opt.relativenumber = true
 lvim.keys.normal_mode["|"] = ":vsplit<CR>"
 lvim.keys.normal_mode["-"] = ":split<CR>"
-lvim.transparent_window = true
+-- lvim.transparent_window = true
 
 lvim.plugins = {
   {
     "catppuccin/nvim",
     lazy = false,
     name = "catppuccin",
-    priority = 1000,
+    -- priority = 1000,
     config = function()
       vim.cmd.colorscheme "catppuccin-mocha"
     end
-  }, {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  event = "InsertEnter",
-  config = function()
-    require("copilot").setup({})
-  end
-},
+  },
   {
-  'chomosuke/typst-preview.nvim',
-  lazy = false, -- or ft = 'typst'
-  version = '0.3.*',
-  build = function() require 'typst-preview'.update() end,
-},
-  --   Old typst config
---   {
---   'kaarmu/typst.vim',
---   ft = 'typst',
---   lazy=false,
---   config = function()
---     -- vim.g.typst_pdf_viewer = 'evince'
---     vim.g.typst_pdf_viewer = 'zathura'
---   end
--- },
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({})
+    end
+  },
   {
-  "zbirenbaum/copilot-cmp",
-  config = function()
-    require("copilot_cmp").setup()
-  end
-},{
-    'justinmk/vim-sneak',
+    'chomosuke/typst-preview.nvim',
+    lazy = false, -- or ft = 'typst'
+    version = '0.3.*',
+    build = function() require 'typst-preview'.update() end,
+  },
+  --   Old typst config {
+  --   'kaarmu/typst.vim',
+  --   ft = 'typst',
+  --   lazy=false,
+  --   config = function()
+  --     -- vim.g.typst_pdf_viewer = 'evince'
+  --     vim.g.typst_pdf_viewer = 'zathura'
+  --   end
+  -- },
+  {
+    "zbirenbaum/copilot-cmp",
+    config = function()
+      require("copilot_cmp").setup()
+    end
   },
   {
     "nyoom-engineering/oxocarbon.nvim",
     name = "oxocarbon",
-  }, {
-  "projekt0n/github-nvim-theme",
-  name = "github-theme",
-}, {
-  "EdenEast/nightfox.nvim",
-  name = "nightfox-theme",
-},
-  { "xiyaowong/transparent.nvim" },
-  {"ellisonleao/glow.nvim", config = true, cmd = "Glow"},
+  },
   {
-  "windwp/nvim-ts-autotag",
-  config = function()
-    require("nvim-ts-autotag").setup()
-  end,
-},{
+    "projekt0n/github-nvim-theme",
+    name = "github-theme",
+  },
+  {
+    "EdenEast/nightfox.nvim",
+    name = "nightfox-theme",
+  },
+  {
+    "Biscuit-Theme/nvim",
+    name = "biscuit-theme",
+  },
+  {
+    "DanielEliasib/sweet-fusion",
+    name = 'sweet-fusion',
+    priority = 1000,
+    opts = {
+      -- Set transparent background
+      -- transparency = true,
+      dim_inactive = true,
+      hl_styles = {
+        comments = { italic = true },
+        keywords = { bold = true },
+      },
+    }
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
+  {
     "romgrk/nvim-treesitter-context",
     config = function()
-      require("treesitter-context").setup{
-        enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+      require("treesitter-context").setup {
+        enable = true,   -- Enable this plugin (Can be enabled/disabled later via commands)
         throttle = true, -- Throttles plugin updates (may improve performance)
-        max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
-        patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
+        max_lines = 0,   -- How many lines the window should span. Values <= 0 mean no limit.
+        patterns = {     -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
           -- For all filetypes
           -- Note that setting an entry here replaces all other patterns for this entry.
           -- By setting the 'default' entry below, you can control which nodes you want to
@@ -91,4 +108,8 @@ lvim.plugins = {
       }
     end
   },
+  { "lukas-reineke/indent-blankline.nvim", main = "ibl",  opts = {} },
+  { "justinmk/vim-sneak" },
+  { "xiyaowong/transparent.nvim" },
+  { "ellisonleao/glow.nvim",               config = true, cmd = "Glow" },
 }

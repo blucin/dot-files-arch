4e91956ee8dd304a55ea3658e9f2eedd05f46629
akshar <glaivesharp@gmail.com>
2024-09-15 16:25:33 +0530

    Add some more dots

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -1,5 +1,4 @@
--- Read the docs: https://www.lunarvim.org/docs/configuration
--- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Read the docs: https://www.lunarvim.org/docs/configuration Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
 
@@ -93,6 +92,10 @@ lvim.plugins = {
   {
     "Biscuit-Theme/nvim",
     name = "biscuit-theme",
+  },
+  {
+    "mellow-theme/mellow.nvim",
+    name = "mellow-theme",
   },
   {
     "DanielEliasib/sweet-fusion",
@@ -251,6 +254,9 @@ lvim.plugins = {
         img_folder = "Files",
       },
     },
+  },
+  {
+    "tpope/vim-surround",
   },
   {
     "kawre/leetcode.nvim",

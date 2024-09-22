ad1c8dbd0590732db8a0aad8ee491f7003525662
Nihar <niharp362004.ni@gmail.com>
2024-09-22 12:38:54 +0530

    Add media controller

---
+++ rofi/.config/rofi/mpd/mpd.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Get the bar PID for Polybar updates
+bar_pid=$(pgrep -a "polybar" | grep "cat-configs" | cut -d" " -f1)
+
+# Player settings
+players="spotify,%any,firefox,chromium,brave,mpd"
+player_status=$(playerctl -p $players status)
+
+# Function to update Polybar
+update_hooks() {
+  echo "$1" | while IFS= read -r id; do
+    polybar-msg -p "$id" hook music-play-pause "$2" 1>/dev/null 2>&1
+  done
+}
+
+# Get the currently playing title
+current_title() {
+    if [[ "$player_status" == "Playing" ]]; then
+        playerctl -p $players metadata --format '{{ artist }} - {{ title }}'
+    else
+        echo "No music playing"
+    fi
+}
+
+# Rofi options
+option_play_pause=""
+option_next=""
+option_prev=""
+
+# Rofi command with the currently playing title
+rofi_cmd() {
+    local title=$(current_title)
+    rofi -theme "$HOME/.config/rofi/mpd/mpd.rasi" -dmenu \
+        -p "$title" \
+        <<< "$option_play_pause
+$option_prev
+$option_next"
+}
+
+# Execute command based on Rofi choice
+run_cmd() {
+    case "$1" in
+        "$option_play_pause")
+            playerctl -p $players play-pause ;;
+        "$option_next")
+            playerctl -p $players next ;;
+        "$option_prev")
+            playerctl -p $players previous ;;
+    esac
+}
+
+# Run Rofi and execute chosen command
+chosen="$(rofi_cmd)"
+run_cmd "$chosen"
--- rofi/.config/rofi/powermenu/powermenu.sh
+++ rofi/.config/rofi/powermenu/powermenu.sh
@@ -1,17 +1,7 @@
 #!/usr/bin/env bash
 
-## Author : Aditya Shakya (adi1090x)
-## Github : @adi1090x
-#
-## Rofi   : Power Menu
-#
-## Available Styles
-#
-## style-1   style-2   style-3   style-4   style-5
-## style-6   style-7   style-8   style-9   style-10
-
 # Current Theme
-dir="$HOME/.config/rofi"
+dir="$HOME/.config/rofi/powermenu"
 theme="powermenu"
 
 # CMDs
